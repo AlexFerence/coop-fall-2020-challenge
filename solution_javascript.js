9a24bbcf488d7de3ad0621fe1ad0b7fcae54c7e4
Alex Ference <alexference23@gmail.com>
2020-07-10 10:31:59 -0700

    init

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,12 +1,41 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.history = []
+    this.index = 0
+  }
+  add(num) {
+    this.value = this.value + num
+    this.history.push(this.value)
+    this.index = this.index + 1
+
+    //check if there is something ahead in the chain and delete if possible
+  }
+  subtract(num) {
+    this.value = this.value - num
+    this.history.push(this.value)
+    this.index = this.index + 1
+  }
+  undo() {
+    this.value = this.history[this.index - 1]
+    this.index = this.index = 1
+
+  }
+  redo() {
+    //check if there is a redo allowed
+    if (this.history.index < this.history.length - 1) {
+      //you will be allowed to redo
+      this.value = this.history[this.index + 1]
+      this.index = this.index + 1
+
+    }
+    else {
+      //not possible to redo
+      
+    }
+
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
   bulk_undo(num) {}
   bulk_redo(num) {}
 }
