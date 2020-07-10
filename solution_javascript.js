e926a1f9ade4a779cb75153d25fc8ca4f9545cc0
Alex Ference <alexference23@gmail.com>
2020-07-10 11:08:10 -0700

    first 5 cases work

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,42 +1,67 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
-    this.history = []
+    this.history = [true, 0]
     this.index = 0
   }
   add(num) {
+    if (this.history.length - 1 > this.index) {
+      this.history[this.index] = [true, num]
+    } 
+    else {
+      this.history.push([true, num])
+    }
     this.value = this.value + num
-    this.history.push(this.value)
     this.index = this.index + 1
-
+    
     //check if there is something ahead in the chain and delete if possible
   }
   subtract(num) {
+    if (this.history.length - 1 > this.index) {
+      this.history[this.index] = [false, num]
+    } 
+    else {
+      this.history.push([false, num])
+    }
     this.value = this.value - num
-    this.history.push(this.value)
     this.index = this.index + 1
+   
   }
   undo() {
-    this.value = this.history[this.index - 1]
-    this.index = this.index = 1
-
+    var recentChange = this.history[this.index][1]
+    if (this.history[this.index][0]) {
+      this.value = this.value - recentChange
+    }
+    else {
+      this.value = this.value + recentChange
+    }
+    this.index = this.index - 1 
   }
   redo() {
     //check if there is a redo allowed
-    if (this.history.index < this.history.length - 1) {
+    if (this.index < this.history.length - 1) {
+      var recentChange = this.history[this.index + 1][1]
+      if (this.history[this.index][0]) {
+        this.value = this.value + recentChange
+      }
+      else {
+        this.value = this.value - recentChange
+      }
       //you will be allowed to redo
-      this.value = this.history[this.index + 1]
       this.index = this.index + 1
-
     }
     else {
       //not possible to redo
+    }
+  }
+
+  bulk_undo(num) {
+    var numGoBack = (this.history.length -1) - this.index
+    if (true) {
       
     }
 
   }
-
-  bulk_undo(num) {}
   bulk_redo(num) {}
 }
 
